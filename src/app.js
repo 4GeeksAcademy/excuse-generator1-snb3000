4622eba5a1ec1a520e606d9eadb921272bae8327
Shafik Bahou <snb3000@gmail.com>
2024-06-04 01:26:49 +0000

    excuse code updates

--- src/app.js
+++ src/app.js
@@ -6,21 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   function generateRandomSentence() {
-    function chooseRandomItem(arr) {
-      const randomIndex = Math.floor(Math.random() * arr.length);
-      return arr[randomIndex]; 
-    }
-  
     let who = ["The dog", "My grandma", "His turtle", "My bird"];
     let action = ["ate", "peed", "crushed", "broke"];
     let what = ["my homework", "the keys", "the car"];
-    let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
-  
-    let sentence = chooseRandomItem(who) + " " + chooseRandomItem(action) + " " + chooseRandomItem(what) + " " + chooseRandomItem(when);
-    document.getElementById('excuse').innerText = sentence;
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying",
+    ];
+
+    let sentence =
+      chooseRandomItem(who) +
+      " " +
+      chooseRandomItem(action) +
+      " " +
+      chooseRandomItem(what) +
+      " " +
+      chooseRandomItem(when);
+
+    document.getElementById("excuse").textContent = sentence;
   }
 
-  generateRandomSentence(); // Call the function to generate a random sentence
-}
+  function chooseRandomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  generateRandomSentence();
+};
