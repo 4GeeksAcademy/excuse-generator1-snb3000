8d3014ca4da8e90c2a291166753c3dcdbd46ff51
Shafik Bahou <snb3000@gmail.com>
2024-06-04 00:20:08 +0000

    updated app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function generateRandomSentence() {
+    function chooseRandomItem(arr) {
+      const randomIndex = Math.floor(Math.random() * arr.length);
+      return arr[randomIndex]; 
+    }
+  
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
+  
+    let sentence = chooseRandomItem(who) + " " + chooseRandomItem(action) + " " + chooseRandomItem(what) + " " + chooseRandomItem(when);
+    document.getElementById('excuse').innerText = sentence;
+  }
+
+  generateRandomSentence(); // Call the function to generate a random sentence
+}
